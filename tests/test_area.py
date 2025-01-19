c8f2b96845c5a2cd820bfcf80bd6a062caf3c40a
Jason Swift <anemele@outlook.com>
2025-01-19 23:33:06 +0800

    new branch: dev; try to make partial mirage tank;

---
+++ tests/test_area.py
@@ -0,0 +1,32 @@
+from mirage_tank.area import convex_hull
+
+
+def f(sample):
+    return [point.to_tuple() for point in convex_hull(sample)]
+
+
+def test_convex_hull():
+    points = [
+        (0, 3),
+        (2, 2),
+        (1, 1),
+        (2, 1),
+        (2, 0),
+        (1, 0),
+        (0, 0),
+        (3, 3),
+        (3, 2),
+        (3, 1),
+    ]
+    expected = [(0, 0), (2, 0), (3, 1), (3, 3), (0, 3)]
+
+    assert f(points) == expected
+
+
+def plot_points(points: list[tuple[float, float]]):
+    import numpy as np
+    from matplotlib.pyplot import scatter, show
+
+    pp = np.array(points)
+    scatter(pp[:, 0], pp[:, 1])
+    show()
